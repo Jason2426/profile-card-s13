369fd76b5190f6c73cb51bcda8db1a931ff7a7c4
Jason <jay.jpy.wong@gmail.com>
2024-09-10 22:20:47 +0200

    Feature : Created Link component

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let label="";
+    export let href="";
+</script>
+
+<li>
+    <a href={href}>
+        {label}
+    </a>
+</li>
+
+<style>
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+</style>
